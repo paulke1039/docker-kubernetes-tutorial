674270114df83535782d8eefbf13102758f926d6
Paul Ke <paulke@unknown2477031FA044.attlocal.net>
2018-12-12 16:36:37 -0500

    updated message

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.use('*', (req, res, next) => {
-	res.send('<H1>Hello from express app </H1>')
+	res.send('<H1>Hello from UPDATED express app </H1>')
 })
 app.listen(4000, () => {
     console.log('Express app listening on port 4000')
